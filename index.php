cb5c8f8bb403d542362ca1cfc3b085e89457890d
Miguel Cortes <miguel.cortes@decameron.com>
2016-10-03 15:06:47 -0500

    Se arregla index.php agregando campo usuario

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 $resId = '';
 if(isset($_POST['host'])){
-        $format = "host=%s port=%d dbname=%s user=%s password=%s";
-        $conn_string = sprintf($format, $_POST['host'], $_POST['port'], $_POST['dbname'], $_POST['user'], $_POST['password']);
-        $dbConn = pg_connect($conn_string);
-        $resId = print_r($dbConn, true);
+            $format = "host=%s port=%d dbname=%s user=%s password=%s";
+                    $conn_string = sprintf($format, $_POST['host'], $_POST['port'], $_POST['dbname'], $_POST['user'], $_POST['password']);
+                            $dbConn = pg_connect($conn_string);
+                            $resId = print_r($dbConn, true);
 }
 
 ?>
 <!Doctype html>
 <html>
 <head>
-        
+
 </head>
 <body>
 <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
@@ -21,12 +21,16 @@ if(isset($_POST['host'])){
 <input type="number" value="5432" name="port" /><br />
 <label for="dbname">Nombre DB</label><br />
 <input type="text" value="postgres" name="dbname" /><br />
+<label for="user">Usuario</label><br />
+<input type="text" value="postgres" name="user" /><br />
 <label for="password">Pass</label><br />
 <input type="password" value="" name="password" /><br />
 <input type="submit" value="Conectar" />
 </form>
-<div>
+<br />
+<h1>
 <?php echo $resId ?>
-</div>
-</body>    
-</html>	
+</h1>
+</body>
+</html>
+
