f403f248a9812aa163300264737c645eab385244
Miguel Cortes <miguel.cortes@decameron.com>
2016-10-03 14:27:49 -0500

    Se agrega formulario para los parametros de conexion a la base de datos

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
-$conn_string = "host=172.17.0.3 port=5432 dbname=postgres user=postgres password=";
-$dbconn4 = pg_connect($conn_string);
-var_dump($dbconn4);
+$resId = '';
+if(isset($_POST['host'])){
+        $format = "host=%s port=%d dbname=%s user=%s password=%s";
+        $conn_string = sprintf($format, $_POST['host'], $_POST['port'], $_POST['dbname'], $_POST['user'], $_POST['password']);
+        $dbConn = pg_connect($conn_string);
+        $resId = print_r($dbConn, true);
+}
+
+
+<!Doctype html>
+<html>
+<head>
+        
+</head>
+<body>
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+<label for="host">Host</label><br />
+<input type="text" value="172.17.0.3" name="host" /><br />
+<label for="port">Port</label><br />
+<input type="number" value="5432" name="port" /><br />
+<label for="dbname">Nombre DB</label><br />
+<input type="text" value="postgres" name="dbname" /><br />
+<label for="password">Pass</label>
+<input type="password" value="" name="password" /><br />
+<input type="submit" value="Conectar" />
+</form>
+<div>
+<?php echo $resId ?>
+</div>
+</body>    
+</html>	
